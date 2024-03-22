e75d1599c366d3d931fda97fb4dfa8cf76ec75cc
Huyền Trang <98087513+PhamVuHuyenTrang@users.noreply.github.com>
2024-03-22 23:57:00 +0700

    Update fan_net_tiny.sh

--- scripts/fan_vit/fan_net_tiny.sh
+++ scripts/fan_vit/fan_net_tiny.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port=10001 \
-	--use_env /lustre/scratch/client/vinai/users/trangpvh1/repo/FAN/main.py  --data_dir /cm/archive/thoth/dataset/ImageNet/ --model fan_tiny_12_p16_224 -b 128 --sched cosine --epochs 300 \
+	--use_env /lustre/scratch/client/vinai/users/trangpvh1/repo/FAN/main.py  --data_dir /lustre/scratch/client/vinai/users/trangpvh1/repo/datasets/imagenet/ --model fan_tiny_12_p16_224 -b 128 --sched cosine --epochs 300 \
 	--opt adamw -j 16 --warmup-lr 1e-6 --warmup-epochs 5  \
 	--model-ema-decay 0.99992 --aa rand-m9-mstd0.5-inc1 --remode pixel \
 	--reprob 0.25 --lr 20e-4 --min-lr 1e-6 --weight-decay .05 --drop 0.0 \
